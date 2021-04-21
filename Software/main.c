93710fdfc524fd1c5a57894459db8ad41ecd47f4
jjhorton <jjhorton@gmail.com>
2021-04-21 21:53:26 +0100

    Change code to be downward binary counter in uint8_t datatype

--- Software/main.c
+++ Software/main.c
@@ -6,33 +6,36 @@
 
 #include "pico/stdlib.h"
 
-#define look_up_table_length = 256;
+#define LOOKUP_TABLE_LENGTH 256
 
 int main() {
 		const uint LED_PINs[] = {0,1,2,3,4,5,6,7};
 		const uint CLK_PIN = 9;
 
+		uint8_t lookup_table[LOOKUP_TABLE_LENGTH];
+
+		for(int x; x<LOOKUP_TABLE_LENGTH; x++){
+			lookup_table[x] = LOOKUP_TABLE_LENGTH-x;
+		}
 
 		for(int x = 0; x<8; x++) {
 				gpio_init(LED_PINs[x]);
 				gpio_set_dir(LED_PINs[x], GPIO_OUT);
 		}
 
-		uint temp = 0;
+		uint8_t temp = 0;
+		uint counter = 0;
 
 		while (true) {
-			temp = 0;
+
+			//temp = lookup_table[counter];
 			for(int x = 0; x<8; x++) {
-        gpio_put(LED_PINs[x], temp);
-				temp = !temp;
+        gpio_put(LED_PINs[x], (lookup_table[counter] >> x) & 1U);
 			}
 			sleep_ms(250);
+			counter++;
 
-			temp = 1;
-			for(int x = 0; x<8; x++) {
-        gpio_put(LED_PINs[x], temp);
-				temp = !temp;
-			}
-      sleep_ms(250);
+			if(counter>LOOKUP_TABLE_LENGTH){ counter = 0; }
+
     }
 }
