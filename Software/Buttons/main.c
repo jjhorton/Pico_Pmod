23429e927c1bc5087ccc0f721587de57469e4460
jjhorton <jjhorton@gmail.com>
2021-04-30 21:51:15 +0100

    Update the copyright notice

--- Software/Buttons/main.c
+++ Software/Buttons/main.c
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ * Copyright (c) 2021 James Horton.
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ *
  */
 
 #include "pico/stdlib.h"
@@ -13,6 +13,7 @@ int main() {
 		//const uint SEVEN_SEGMENT_PINS[] = {9,10,13,15,8,11,12,14};
 		//const uint SEVEN_SEGMENT_PINS[] = {12,11,8,15,13,10,9,14};
 		const uint SEVEN_SEGMENT_PINS[] = {9,11,13,15,8,10,12,14};
+		const uint BUTTON_PINS[] = {2,3,16,17,18,19,20,21}
 		const uint CLK_PIN = 9;
 
 		uint8_t lookup_table[LOOKUP_TABLE_LENGTH];
@@ -21,17 +22,23 @@ int main() {
 			lookup_table[x] = LOOKUP_TABLE_LENGTH-x;
 		}
 
+		// The output to the system
 		for(int x = 0; x<8; x++) {
 				gpio_init(SEVEN_SEGMENT_PINS[x]);
 				gpio_set_dir(SEVEN_SEGMENT_PINS[x], GPIO_OUT);
 				gpio_put(SEVEN_SEGMENT_PINS[x],0);
 		}
 
+		// Set the pins to input
+		for(int x = 0; x<8; x++) {
+				gpio_init(BUTTON_PINS[x]);
+				gpio_set_dir(BUTTON_PINS[x], GPIO_IN);
+		}
+
 		uint8_t temp = 0b0000000;
 		uint counter = 0;
-		uint counter1 = 0;
-		uint counter2 = 0;
-
+		uint8_t input1 = 0;
+		uint8_t input2 = 0;
 
 		uint8_t characters[] = {0b01000000,	// 0
 														0b01111001,	// 1
@@ -52,9 +59,13 @@ int main() {
 
 		while (true) {
 
+			// Read the values from the buttons
+			for(int x = 0; x<8; x++) {
+				gpio_get(SEVEN_SEGMENT_PINS[x]);
+			}
 			//for the first digit
 			for(int x = 0; x<7; x++) {
-        gpio_put(SEVEN_SEGMENT_PINS[x], (characters[counter1] >> x) & 1U);
+        gpio_put(SEVEN_SEGMENT_PINS[x], (characters[input1] >> x) & 1U);
 			}
 
 			gpio_put(SEVEN_SEGMENT_PINS[7], 1);
@@ -62,15 +73,11 @@ int main() {
 
 			//for the second digit
 			for(int x = 0; x<7; x++) {
-				gpio_put(SEVEN_SEGMENT_PINS[x], (characters[counter2] >> x) & 1U);
+				gpio_put(SEVEN_SEGMENT_PINS[x], (characters[input2] >> x) & 1U);
 			}
 
 			gpio_put(SEVEN_SEGMENT_PINS[7], 0);
 			sleep_ms(10);
-			counter++;
 
-			if(counter>LOOKUP_TABLE_LENGTH){ counter = 0; counter1++;}
-			if(counter1>15){ counter1=0; counter2++;}
-			if(counter2>15){ counter2=0;}
     }
 }
