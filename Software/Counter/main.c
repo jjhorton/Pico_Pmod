4bffa812a4cdef768dd02178a09d5edaed70f566
jjhorton <jjhorton@gmail.com>
2021-05-10 21:27:13 +0100

    Remove the unused element of the main function

--- Software/Counter/main.c
+++ Software/Counter/main.c
@@ -1,25 +1,15 @@
 /**
  * Copyright (c) 2021 James Horton
  *
- * 
+ *
  */
 
 #include "pico/stdlib.h"
 
-#define LOOKUP_TABLE_LENGTH 100
-
+#define PAUSE_LENGTH 100;
 
 int main() {
-		//const uint SEVEN_SEGMENT_PINS[] = {9,10,13,15,8,11,12,14};
-		//const uint SEVEN_SEGMENT_PINS[] = {12,11,8,15,13,10,9,14};
 		const uint SEVEN_SEGMENT_PINS[] = {9,11,13,15,8,10,12,14};
-		const uint CLK_PIN = 9;
-
-		uint8_t lookup_table[LOOKUP_TABLE_LENGTH];
-
-		for(int x; x<LOOKUP_TABLE_LENGTH; x++){
-			lookup_table[x] = LOOKUP_TABLE_LENGTH-x;
-		}
 
 		for(int x = 0; x<8; x++) {
 				gpio_init(SEVEN_SEGMENT_PINS[x]);
@@ -27,7 +17,6 @@ int main() {
 				gpio_put(SEVEN_SEGMENT_PINS[x],0);
 		}
 
-		uint8_t temp = 0b0000000;
 		uint counter = 0;
 		uint counter1 = 0;
 		uint counter2 = 0;
@@ -69,7 +58,7 @@ int main() {
 			sleep_ms(10);
 			counter++;
 
-			if(counter>LOOKUP_TABLE_LENGTH){ counter = 0; counter1++;}
+			if(counter>PAUSE_LENGTH){ counter = 0; counter1++;}
 			if(counter1>15){ counter1=0; counter2++;}
 			if(counter2>15){ counter2=0;}
     }
